aa63b2a17c96d79dc040d49379bcb0a42fb2be59
brycekrause <brycekrause@icloud.com>
2026-01-04 19:38:09 -0600

    implement svelte and basic canvas

---
+++ fractal-site/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Canvas from './lib/Canvas.svelte';
+
+  function draw(ctx) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(100, 100, 50, 50);
+  }
+</script>
+
+<h1>Fractal Playground</h1>
+<Canvas {draw} />
---
+++ fractal-site/src/lib/Canvas.svelte
@@ -0,0 +1,19 @@
+<script>
+  let canvas;
+
+  export let draw = () => {};
+
+  $: if (canvas) {
+    const ctx = canvas.getContext("2d");
+    draw(ctx);
+  }
+</script>
+
+<canvas bind:this={canvas} width="600" height="600"></canvas>
+
+<style>
+  canvas {
+    border: 1px solid #444;
+    background: #111;
+  }
+</style>
